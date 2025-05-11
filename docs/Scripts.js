333aff052c2e657646d2512d6b14404f29dfed9a
shane0819 <61346940+shane0819@users.noreply.github.com>
2025-05-11 16:08:52 +0800

    Update Scripts.js

--- docs/Scripts.js
+++ docs/Scripts.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('sw.js')
     .then(() => console.log('SW註冊成功'))
     .catch(err => console.log('SW註冊失敗', err));
 }
